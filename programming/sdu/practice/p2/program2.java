3c06d0483bc466fb613d2c884eb790c6f1bee66b
Rasmustex <71628935+Rasmustex@users.noreply.github.com>
2021-01-29 13:53:34 +0100

    Add files via upload

---
+++ programming/sdu/practice/p2/program2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class program2 {
+
+	public static void main(String[] args) {
+		/* switches er rimelig simple og følger strukturen 
+		switch(var) {
+			case n(eller en bestemt string):
+				code
+				break;
+		}
+		*/
+		//obj.toLowerCase er rimelig smart
+		//husk obj.close(); for et Scanner objekt
+	}
+}
